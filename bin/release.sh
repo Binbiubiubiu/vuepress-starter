a99cc2ad3bb2211852473d0d560993b15334a265
lvbin <2632003786@qq.com>
2020-01-08 10:12:16 +0800

    :bookmark: v2.0.3

--- bin/release.sh
+++ bin/release.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
+export CONVENTIONAL_GITHUB_RELEASER_TOKEN=313138610f53d79d11e7c881ec165f51b194ee77
 
 npx gitmoji-changelog
 
 git add -A
-git commit -m ":bookmark: $0"
-git tag $0
+git commit -m ":bookmark: $1"
+git tag $1
 git push origin master --tags
-npx conventional-github-releaser -p angular
+npx conventional-github-releaser -p angular 
